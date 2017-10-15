84631b7cc7490ca9ce6137af89de647e8feac7d9
Sebastian Stricker <sebastian.stricker@technikum-wien.at>
2017-10-15 23:21:23 +0200

    formatting

--- myclient.cpp
+++ myclient.cpp
@@ -12,50 +12,54 @@
 #define BUF 1024
 #define PORT 6543
 
+using namespace std;
+
 int main (int argc, char **argv) {
-  int create_socket;
+  int socket_fd;
   char buffer[BUF];
   struct sockaddr_in address;
   int size;
 
-  if( argc < 2 ){
-     printf("Usage: %s ServerAdresse\n", argv[0]);
-     exit(EXIT_FAILURE);
-  }
+  if( argc < 2 )
+    {
+      printf("Usage: %s ServerAdresse\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
 
-  if ((create_socket = socket (AF_INET, SOCK_STREAM, 0)) == -1)
-  {
-     perror("Socket error");
-     return EXIT_FAILURE;
-  }
+  if ((socket_fd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+      perror("Socket error");
+      return EXIT_FAILURE;
+    }
   
   memset(&address,0,sizeof(address));
   address.sin_family = AF_INET;
   address.sin_port = htons (PORT);
   inet_aton (argv[1], &address.sin_addr);
 
-  if (connect ( create_socket, (struct sockaddr *) &address, sizeof (address)) == 0)
-  {
-     printf ("Connection with server (%s) established\n", inet_ntoa (address.sin_addr));
-     size=recv(create_socket,buffer,BUF-1, 0);
-     if (size>0)
-     {
-        buffer[size]= '\0';
-        printf("%s",buffer);
-     }
-  }
+  if (connect (socket_fd, (struct sockaddr *) &address, sizeof (address)) == 0)
+    {
+      printf ("Connection with server (%s) established\n", inet_ntoa (address.sin_addr));
+      size=recv(socket_fd,buffer,BUF-1, 0);
+      if (size>0)
+        {
+          buffer[size]= '\0';
+          printf("%s",buffer);
+        }
+    }
   else
-  {
-     perror("Connect error - no server available");
-     return EXIT_FAILURE;
-  }
+    {
+      perror("Connect error - no server available");
+      return EXIT_FAILURE;
+    }
 
-  do {
-     printf ("Send message: ");
-     fgets (buffer, BUF, stdin);
-     send(create_socket, buffer, strlen (buffer), 0);
-  } 
+  do
+    {
+      printf ("Send message: ");
+      fgets (buffer, BUF, stdin);
+      send(socket_fd, buffer, strlen (buffer), 0);
+    } 
   while (strcmp (buffer, "quit\n") != 0);
-  close (create_socket);
+  close (socket_fd);
   return EXIT_SUCCESS;
 }
--- myserver.cpp
+++ myserver.cpp
@@ -15,56 +15,60 @@
 using namespace std;
 
 int main (void) {
-  int create_socket, new_socket;
+  int server_socket_fd, client_socket_fd;
   socklen_t addrlen;
   char buffer[BUF];
   int size;
   struct sockaddr_in address, cliaddress;
 
-  create_socket = socket (AF_INET, SOCK_STREAM, 0);
+  server_socket_fd = socket (AF_INET, SOCK_STREAM, 0);
 
   memset(&address, 0, sizeof(address));
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons (PORT);
 
-  if (bind ( create_socket, (struct sockaddr *) &address, sizeof (address)) != 0) {
-     perror("bind error");
-     return EXIT_FAILURE;
-  }
-  listen (create_socket, 5);
+  if (bind ( server_socket_fd, (struct sockaddr *) &address, sizeof (address)) != 0)
+    {
+      perror("bind error");
+      return EXIT_FAILURE;
+    }
+  listen (server_socket_fd, 5);
   
   addrlen = sizeof (struct sockaddr_in);
 
-  while (1) {
-     printf("Waiting for connections...\n");
-     new_socket = accept ( create_socket, (struct sockaddr *) &cliaddress, &addrlen );
-     if (new_socket > 0)
-     {
-        printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr),ntohs(cliaddress.sin_port));
-        strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
-        send(new_socket, buffer, strlen(buffer),0);
-     }
-     do {
-        size = recv (new_socket, buffer, BUF-1, 0);
-        if( size > 0)
+  while (1)
+    {
+      printf("Waiting for connections...\n");
+      client_socket_fd = accept ( server_socket_fd, (struct sockaddr *) &cliaddress, &addrlen );
+      if (client_socket_fd > 0)
         {
-           buffer[size] = '\0';
-           printf ("Message received: %s\n", buffer);
+          printf ("Client connected from %s:%d...\n", inet_ntoa (cliaddress.sin_addr),ntohs(cliaddress.sin_port));
+          strcpy(buffer,"Welcome to myserver, Please enter your command:\n");
+          send(client_socket_fd, buffer, strlen(buffer),0);
         }
-        else if (size == 0)
+      do
         {
-           printf("Client closed remote socket\n");
-           break;
+          size = recv (client_socket_fd, buffer, BUF-1, 0);
+          if( size > 0)
+            {
+              buffer[size] = '\0';
+              printf ("Message received: %s\n", buffer);
+            }
+          else if (size == 0)
+            {
+              printf("Client closed remote socket\n");
+              break;
+            }
+          else
+            {
+              perror("recv error");
+              return EXIT_FAILURE;
+            }
         }
-        else
-        {
-           perror("recv error");
-           return EXIT_FAILURE;
-        }
-     } while (strncmp (buffer, "quit", 4)  != 0);
-     close (new_socket);
-  }
-  close (create_socket);
+      while (strncmp (buffer, "quit", 4)  != 0);
+      close (client_socket_fd);
+    }
+  close (server_socket_fd);
   return EXIT_SUCCESS;
 }
