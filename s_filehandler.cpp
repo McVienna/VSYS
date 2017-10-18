e854835337d0593cac617a4d46a09caffe616812
Markus <markus.vogl96@hotmail.com>
2017-10-18 22:49:14 +0200

    adde function for creating user dir

--- s_filehandler.cpp
+++ s_filehandler.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "s_filehandler.h"
+#include "protocols.h"
 #include <experimental/filesystem>
 
 namespace fs = std::experimental::filesystem::v1;
@@ -24,8 +25,20 @@ filehandler::~filehandler(){
 
 }
 
-void filehandler::create_usr_dir(std::string usr, std::string path) {
+void filehandler::create_usr_dir(Send_prot * instanciate_message) {
     ///should create subdirectory if message recieved -> sub_dir name = username
+    std::string user_path = this->path + "/" + this->user;
+    this->user = instanciate_message->return_sender();
+
+    if(!(fs::is_directory(user_path)))
+    {
+        fs::create_directories(user_path);
+        this->path = user_path;
+    }
+    else
+    {
+        this->path = user_path;
+    }
 }
 
 std::string filehandler::return_path()
